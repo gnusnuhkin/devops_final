1acbd58ab1a3ba92278ded8f06d8775a5ab1b045
gnusnuhkin <nsung@unomaha.edu>
2026-05-01 01:41:26 -0500

    Add Terraform stage 3 cleanup configuration

---
+++ terraform/stage3/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name       = "big-tf-state-acmp2400"
+    key                  = "nsung"
+    use_azuread_auth     = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
